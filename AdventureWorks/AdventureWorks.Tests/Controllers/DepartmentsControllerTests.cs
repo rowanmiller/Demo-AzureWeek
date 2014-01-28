156490d0e26fa883f4102f36ffb97988a95fd1c5
RoMiller <rowmil@microsoft.com>
2014-01-28 12:04:02 -0800

    Base solution to start demo Basically just an empty MVC application with a few NuGet packages installed

---
+++ AdventureWorks/AdventureWorks.Tests/Controllers/DepartmentsControllerTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using AdventureWorks.Controllers;
+using AdventureWorks.Tests;
+
+namespace TR1.Tests.Controllers
+{
+    [TestClass]
+    public class DepartmentsControllerTests
+    {
+        [TestMethod]
+        public void IndexSortedByName()
+        {
+
+        }
+    }
+}
